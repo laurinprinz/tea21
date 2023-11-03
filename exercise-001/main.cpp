c07c50af0bc0d46f688d896987de8f14bf44c133
Christian Ege <ch@ege.io>
2023-11-03 15:09:37 +0100

    added printout of segments

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -6,7 +6,13 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
-int foo;
+
+// .bss segment
+int bss;
+// .data segment
+int data = 4711;
+// .rodata segmenet
+const int rodata = 42;
 
 auto main(int argc, char **argv) -> int
 {
@@ -15,7 +21,17 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", "hello");
+    fmt::print("Value of variable bss {} address of variable bss {}\n",
+    bss,
+    fmt::ptr(&bss));
+
+    fmt::print("Value of variable data {} address of variable data {}\n",
+    data,
+    fmt::ptr(&data));
+
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n",
+    rodata,
+    fmt::ptr(&rodata));
 
     return 0; /* exit gracefully*/
 }
