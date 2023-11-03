1477549c8d0d13706827a4bac1f55e43b9b63a02
Christian Ege <ch@ege.io>
2023-11-03 15:18:17 +0100

    double the printout

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -9,10 +9,13 @@
 
 // .bss segment
 int bss;
+int bss2;
 // .data segment
 int data = 4711;
+int data2 = 4709;
 // .rodata segmenet
 const int rodata = 42;
+const int rodata2 = 99;
 
 
 auto main(int argc, char **argv) -> int
@@ -25,14 +28,24 @@ auto main(int argc, char **argv) -> int
     fmt::print("Value of variable bss {} address of variable bss {}\n",
     bss,
     fmt::ptr(&bss));
+    fmt::print("Value of variable bss2 {} address of variable bss2 {}\n",
+    bss2,
+    fmt::ptr(&bss2));
+
 
     fmt::print("Value of variable data {} address of variable data {}\n",
     data,
     fmt::ptr(&data));
+    fmt::print("Value of variable data2 {} address of variable data2 {}\n",
+    data2,
+    fmt::ptr(&data2));
 
     fmt::print("Value of variable rodata {} address of variable rodata {}\n",
     rodata,
     fmt::ptr(&rodata));
+    fmt::print("Value of variable rodata2 {} address of variable rodata2 {}\n",
+    rodata2,
+    fmt::ptr(&rodata2));
 
     return 0; /* exit gracefully*/
 }
